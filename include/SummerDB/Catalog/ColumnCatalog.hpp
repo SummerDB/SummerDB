fe4f68037087a5e32e49c5ce9f513f82f13b75d4
CanftIn <wwc7033@gmail.com>
2023-07-12 13:57:24 +0800

    add column catalog define

---
+++ include/SummerDB/Catalog/ColumnCatalog.hpp
@@ -0,0 +1,32 @@
+#ifndef SUMMERDB_CATALOG_COLUMN_CATALOG_HPP
+#define SUMMERDB_CATALOG_COLUMN_CATALOG_HPP
+
+#include "SummerDB/Common/InternalTypes.hpp"
+#include "SummerDB/Common/Types/Value.hpp"
+
+namespace SummerDB {
+
+//! A column of a table.
+class ColumnDefinition {
+ public:
+  ColumnDefinition(std::string name, TypeId type);
+  ColumnDefinition(std::string name, TypeId type, Value default_value);
+
+  //! The name of the entry
+  std::string name;
+  //! The index of the column in the table
+  size_t oid;
+  //! The type of the column
+  TypeId type;
+  //! Whether or not the column has a default value
+  bool has_default;
+  //! The default value of the column (if any)
+  Value default_value;
+
+  virtual std::string ToString() const { return std::string(); }
+  virtual ~ColumnDefinition(){};
+};
+
+}  // namespace SummerDB
+
+#endif  // SUMMERDB_CATALOG_COLUMN_CATALOG_HPP
