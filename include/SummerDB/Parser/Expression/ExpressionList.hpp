1371db5bc909bf4156ef20ec3e5fcb10f5164063
CanftIn <wwc7033@gmail.com>
2023-07-24 12:00:04 +0800

    add expression list

---
+++ include/SummerDB/Parser/Expression/ExpressionList.hpp
@@ -0,0 +1,8 @@
+#ifndef SUMMERDB_PARSER_EXPRESSION_EXPRESSION_LIST_HPP
+#define SUMMERDB_PARSER_EXPRESSION_EXPRESSION_LIST_HPP
+
+#include "SummerDB/Parser/Expression/AggregateExpression.hpp"
+#include "SummerDB/Parser/Expression/TableRefExpression.hpp"
+#include "SummerDB/Parser/Expression/BaseTableRefExpression.hpp"
+
+#endif  // SUMMERDB_PARSER_EXPRESSION_EXPRESSION_LIST_HPP
